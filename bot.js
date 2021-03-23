f241e23fd6c18d55fcd58dc178457cee9d6369f8
Mid1k <ilya292935118@mail.ru>
2021-03-23 21:33:11 +0300

    bot

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+        console.log('Запустился бот ' + bot.user.tag);
+    })
+
+    bot.on("message", async message => {
+      if (message.content.startsWith("<eval") || message.content.startsWith("<e")) {
+          if(!message.member.hasPermission(8)) return
+        let args = message.content.split(" ");
+        args = args.slice(1);
+        try {
+          message.channel.send(require('util').inspect(eval(args.join(" ")), {depth: 1, colorize: true}), {code: 'js'})
+        } catch(e) {
+          message.channel.send(e, {code: 'js'})
+          message.react('🆗')
+        }
+      }
+    });
+
+bot.login('token');
